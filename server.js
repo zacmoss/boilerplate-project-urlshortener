fa8e77985141191238a0137d6e22076a1468738d
ZacMoss <zachary.moss@ymail.com>
2018-09-11 13:06:32 -0500

    Added Solution

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 var cors = require('cors');
 
@@ -15,6 +17,7 @@ var port = process.env.PORT || 3000;
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -30,6 +33,52 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+
+
+app.post('/api/shorturl/new', function(req, res) {
+  let url = req.body.url;
+  let number = (Math.random() * 99).toFixed(0);
+  let error = undefined;
+  //console.log(url.indexOf('https://'));
+  if (url.indexOf('https://') === -1) {
+    error = "invalid url";
+    console.log("no https://");
+    res.json({"error": "invalid url"});
+  } else {
+    let short = url.slice(8);
+    //console.log("short" + short);
+    dns.lookup(short, function(err) {
+      if (err) {
+        error = "invalid url";
+        res.json({"error": "invalid url"});
+      } else {
+        res.json({"original_url": url, "shortened_url": number});
+        app.get('/api/shorturl/' + number, function(req, res) {
+          // link to url
+          res.redirect(url);
+        });
+      }
+    });
+  }
+  console.log(url);
+  console.log(number);
+  /*
+  if (error) {
+    res.json({"error": "invalid url"});
+  } else {
+    res.json({"original_url": url, "shortened_url": number});
+    app.get('/api/shorturl/' + number, function(req, res) {
+      // link to url
+      res.redirect(url);
+      //console.log('worked');
+    });
+  }
+  */
+  
+});
+
+
 
 
 app.listen(port, function () {
